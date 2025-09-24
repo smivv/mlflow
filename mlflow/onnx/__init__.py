b0df7d8fc316386c8ce1b73f3d054ef606971504
Vladimir Smirnov <smivvla@gmail.com>
2025-09-24 16:49:31 +0300

    Update onnx/__init__.py
    Add `**kwargs` for consistency with `pytorch` flavor.
    
    Signed-off-by: Vladimir Smirnov <smivvla@gmail.com>

--- mlflow/onnx/__init__.py
+++ mlflow/onnx/__init__.py
@@ -102,6 +102,7 @@ def save_model(
     onnx_session_options=None,
     metadata=None,
     save_as_external_data=True,
+    **kwargs,
 ):
     """
     Save an ONNX model to a path on the local file system.
@@ -149,6 +150,7 @@ def save_model(
             https://onnxruntime.ai/docs/api/python/api_summary.html#sessionoptions
         metadata: {{ metadata }}
         save_as_external_data: Save tensors to external file(s).
+        kwargs: kwargs to pass to ``onnx.save`` method.
     """
     import onnx
 
@@ -174,9 +176,9 @@ def save_model(
 
     # Save onnx-model
     if Version(onnx.__version__) >= Version("1.9.0"):
-        onnx.save_model(onnx_model, model_data_path, save_as_external_data=save_as_external_data)
+        onnx.save_model(onnx_model, model_data_path, save_as_external_data=save_as_external_data, **kwargs)
     else:
-        onnx.save_model(onnx_model, model_data_path)
+        onnx.save_model(onnx_model, model_data_path, **kwargs)
 
     pyfunc.add_to_model(
         mlflow_model,
@@ -467,6 +469,7 @@ def log_model(
     model_type: str | None = None,
     step: int = 0,
     model_id: str | None = None,
+    **kwargs,
 ):
     """
     Log an ONNX model as an MLflow artifact for the current run.
@@ -525,6 +528,7 @@ def log_model(
         model_type: {{ model_type }}
         step: {{ step }}
         model_id: {{ model_id }}
+        kwargs: kwargs to pass to ``onnx.save`` method.
 
     Returns:
         A :py:class:`ModelInfo <mlflow.models.model.ModelInfo>` instance that contains the
@@ -552,4 +556,5 @@ def log_model(
         model_type=model_type,
         step=step,
         model_id=model_id,
+        **kwargs,
     )
